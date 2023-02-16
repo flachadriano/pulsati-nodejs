aadf9e7f12166bf71af807019bf7c36e25781fcf
Adriano Flach de Araujo <adriano.spintec@oncologiador.corp>
2023-02-16 14:09:01 -0300

    aula 6 banco de dados

---
+++ aula06/usuario.js
@@ -0,0 +1,13 @@
+class Usuario {
+  codigo;
+  nome;
+  sobrenome;
+
+  constructor(codigo, nome, sobrenome) {
+    this.codigo = codigo
+    this.nome = nome;
+    this.sobrenome = sobrenome
+  }
+}
+
+exports.Usuario = Usuario;
